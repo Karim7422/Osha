aeebce1da09cb245adc5578674b071f83f53b9ce
karim7422 <karimhatem7274@gmail.com>
2023-11-29 06:37:11 +0200

    create News Section,Certificates Section , Footer , add arrow up icon and whatsapp icon

--- js/main.js
+++ js/main.js
@@ -1,1 +1,8 @@
+
+document.querySelector(".fa-circle-arrow-up").onclick=function(){
+    window.scrollTo({
+        top:0,
+        behavior:"smooth",
+    })
+}
 document.getElementById("offcanvas").style.top="50%";
