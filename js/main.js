f89e08091b5376157c0c71249dfcf771e753a635
karim7422 <karimhatem7274@gmail.com>
2023-11-29 19:04:20 +0200

    create blogs

--- js/main.js
+++ js/main.js
@@ -1,5 +1,8 @@
-
-document.querySelector(".fa-circle-arrow-up").onclick=function(){
+const arrowUp = document.querySelector(".fa-circle-arrow-up");
+window.onscroll=function(){
+    window.scrollY >=500 ? arrowUp.classList.add("show") : arrowUp.classList.remove("show");
+};
+arrowUp.onclick=function(){
     window.scrollTo({
         top:0,
         behavior:"smooth",
